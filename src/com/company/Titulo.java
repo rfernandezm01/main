8728fb9a239b5ef6cfb2033a0bce9694a1a10757
rfernandezm01 <81315987+rfernandezm01@users.noreply.github.com>
2021-04-12 23:29:34 +0200

    Create Titulo.java

---
+++ src/com/company/Titulo.java
@@ -0,0 +1,10 @@
+package com.company;
+
+
+public class Titulo {
+    void mostrar (String titulo) {
+        System.out.println("----------------");
+        System.out.println("[ "+ titulo +" ]");
+        System.out.println("----------------");
+    }
+}
